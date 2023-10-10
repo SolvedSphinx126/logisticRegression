ffb9f309a903dcebc2d4ddb88e606619524fe9f1
Jeremiah Rhoton <JeremiahHR133@gmail.com>
2023-10-10 18:13:47 -0500

    Add hypothesis function

--- formulas.py
+++ formulas.py
@@ -7,3 +7,7 @@ def sigmoid(z):
         return np.vectorize(lambda x: 1 / (1 + (math.e ** -x)))(z)
     else:
         return 1 / (1 + (math.e ** -z))
+
+def hypothesis(x, theta):
+    if isinstance(theta, np.ndarray) & isinstance(x, np.ndarray):
+        return sigmoid(np.matmul(theta.T, x))
