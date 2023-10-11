5c00df832c71d98cb3a656e2d5af13329f48a840
Jeremiah Rhoton <JeremiahHR133@gmail.com>
2023-10-10 19:44:13 -0500

    Split dataframe into training and validation

--- logisticRegression.py
+++ logisticRegression.py
@@ -21,3 +21,12 @@ while i < len(rawDataVals):
     i += len(dataLabels)
 
 dataframe = pd.DataFrame(formattedDataVals, columns=dataLabels)
+
+traningData = dataframe.sample(frac=.8)
+testData = dataframe.drop(traningData.index)
+
+# Try again if we got unlucky and didn't have an instance of all three classes in the validation set
+if not ("Iris-setosa" in testData["class"].values and "Iris-versicolor" in testData["class"].values and "Iris-virginica" in testData["class"].values):
+    print("Retrying selection of traning and validation data")
+    traningData = dataframe.sample(frac=.8)
+    testData = dataframe.drop(traningData.index)
