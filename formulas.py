6f54b1f1a69e4b953ab3509a893a86ee49800b7c
Mickey Stephenson <mgs0033@uah.edu>
2023-10-11 12:13:12 -0500

    Added confusion matrix formula

--- formulas.py
+++ formulas.py
@@ -20,3 +20,25 @@ def cost(theta, data, y):
         curr_cum += -y[x[0]][0] * math.log(hypothesis(x[1], theta), 10) - (1 - y[x[0]][0]) * math.log(1 - hypothesis(x[1], theta), 10)
     return (1 / m) * curr_cum
 
+#Creates the confusion matrix for each prediction.
+def confusionMatrix(predictedData, actualData):
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    for i in range(predictedData):
+        if (predictedData[i] == 1):
+            if (actualData[i] == 1):
+                #True Positive
+                tp+=1
+            else:
+                #False Positive
+                fp+=1
+        else:
+            if (actualData[i] == 1):
+                #False Negative
+                fn+=1
+            else:
+                #True Negative
+                tn+=1
+    return tp, fp, tn, fn
